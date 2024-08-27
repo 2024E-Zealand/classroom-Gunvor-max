3c30bae20ac38adc4364e6769b941942363d880e
Mathias Birkkjær Nielsen <birkkjaer6@gmail.com>
2024-08-27 14:26:32 +0200

    Added method season

--- ClassRoomNet60/Student.cs
+++ ClassRoomNet60/Student.cs
@@ -20,9 +20,34 @@ namespace ClassRoomNet60
             BirthDay = birthDay;
         }
 
+        public string season(int birthMonth)
+        {
+            switch (birthMonth)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    return "Winter";
+                    case 3:
+                    case 4:
+                    case 5:
+                    return "Spring";
+                    case 6:
+                    case 7:
+                    case 8:
+                    return "Summer";
+                    case 9:
+                    case 10:
+                    case 11:
+                    return "Autumn";
+                default:
+                    return "Not specified";
+            }
+        }
+
         public override string ToString()
         {
-            return $"{{{nameof(Name)}={Name}, {nameof(BirthMonth)}={BirthMonth.ToString()}, {nameof(BirthDay)}={BirthDay.ToString()}}}";
+            return $"{{{nameof(Name)}={Name}, {nameof(BirthMonth)}={BirthMonth.ToString()}, {nameof(BirthDay)}={BirthDay.ToString()}, {season(BirthMonth)}}}";
         }
     }
 }
